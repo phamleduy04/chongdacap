1b67024c16650ed2f9143e32e1283e4decadcf2e
Duy Pham Le <phamleduy04@gmail.com>
2021-03-30 14:50:06 -0500

    push

--- js/content.js
+++ js/content.js
@@ -1,3 +1,31 @@
-$(document).ready(async function() {
-    $('#j83agx80')
-});
+var regex2 = /(https|http):\/\/www.messenger.com\/t\/*/gm;
+var observer = new MutationObserver(function(mutaions) {
+    mutaions.forEach(el => {
+        if (el.target.hasAttribute('aria-label')) processData();
+    });
+})
+
+function processData() {
+    let testArr = Array.from(document.getElementsByClassName('oajrlxb2')).filter(el => el.href && el.href.match(regex2));
+    if (testArr.length == 0) return;
+    testArr.forEach(el => {
+        pChild('test', el);
+    });
+}
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+})
+
+function pChild(content = 'yay', body) {
+    const checkPrevious = body.getElementsByTagName('a');
+    if (checkPrevious.length != 0) {
+        checkPrevious[0].innerText = content;
+        return;
+    }
+    var tag = document.createElement('a');
+    tag.appendChild(document.createTextNode(content));
+    tag.style.color = "red";
+    body.appendChild(tag);
+}
